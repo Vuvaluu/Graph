23d2d043413a1a58d8a11e166e0ad05fe40326fe
Vuvaluu <107201377+Vuvaluu@users.noreply.github.com>
2024-01-29 22:25:57 -0600

    Last commit

--- Grafo/Assets/Scripts/Graph.cs
+++ Grafo/Assets/Scripts/Graph.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class Graph : MonoBehaviour
 {
    List<Edge> edges;
-   [SerializeField] List<GameObject> goNodes;
+   [SerializeField] List<Node> nodes;
    
   void Start()
   {
      edges = new List<Edge>();
+     nodes = new List<Node>();
+
+    //Initialize nodes with random positions between -10 and 10
+      nodes.Add( new Node(Random.Range(-10, 10), Random.Range(-10, 10)));
+      nodes.Add( new Node(Random.Range(-10, 10), Random.Range(-10, 10)));
+      nodes.Add( new Node(Random.Range(-10, 10), Random.Range(-10, 10)));
+      nodes.Add( new Node(Random.Range(-10, 10), Random.Range(-10, 10)));
+      nodes.Add( new Node(Random.Range(-10, 10), Random.Range(-10, 10)));
+      nodes.Add( new Node(Random.Range(-10, 10), Random.Range(-10, 10)));
+      nodes.Add( new Node(Random.Range(-10, 10), Random.Range(-10, 10)));
+      nodes.Add( new Node(Random.Range(-10, 10), Random.Range(-10, 10)));
+      
      Initialize();
   }
 
@@ -17,23 +29,22 @@ public class Graph : MonoBehaviour
   public void Initialize()
   {
    // Compare all nodes with each other to create edges.
-     foreach(GameObject goNode in goNodes)
+     foreach(Node node in nodes)
      {
-       foreach(GameObject goNode2 in goNodes) {
-         if(goNode != goNode2){
+       foreach(Node node2 in nodes) {
+         if(node != node2){
             // Check distance between two nodes.
-            float distance = Vector3.Distance(goNode.transform.position, goNode2.transform.position);
+          float distance = Vector2.Distance(node.pos, node2.pos);
             // If the distance between the two nodes is less than 10, create an edge.
-            if(distance < 10) {
+           if(distance < 10) {
                // Create an edge and add it to the list of edges.
-              edges.Add(new Edge(goNode.GetComponent<Node>(), goNode2.GetComponent<Node>(), (int)distance));
-              Debug.Log(goNode.name + "+" + goNode2.name + "=" + (int)distance);
-
+              edges.Add(new Edge(node, node2, (int)distance));
+              Debug.Log(node.pos.x+ ", " + node.pos.y + "+" + node2.pos.x + ", " + node2.pos.y + "=" + (int)distance);
             }
          }
        }
      }
-    
+            Debug.Log(edges.Count);
   }
 
 }
--- Grafo/Assets/Scripts/Node.cs
+++ Grafo/Assets/Scripts/Node.cs
@@ -2,10 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node : MonoBehaviour
+public class Node
 {
     List<Edge> edges = new List<Edge>();
     [SerializeField] int heuristic;
+    public Vector2 pos;
+
+    public Node(int x, int y)
+    {
+        pos = new Vector2(x, y);
+    }
 
 // Return the value of the heuristic.
     public int GetHeuristic()
